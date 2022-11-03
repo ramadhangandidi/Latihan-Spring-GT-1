5a1b9a9841d28f09034e03be44d861a4c9cf01c6
ramadhangandidi <thinkpad1st@gmail.com>
2022-11-03 18:51:52 +0700

    First Commit

---
+++ latihangt/src/main/java/com/example/latihangt/LatihangtApplication.java
@@ -0,0 +1,13 @@
+package com.example.latihangt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatihangtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatihangtApplication.class, args);
+	}
+
+}
---
+++ latihangt/src/test/java/com/example/latihangt/LatihangtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.latihangt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatihangtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
